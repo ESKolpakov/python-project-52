e8ab8662a247a3043017319d1a0cf7d2a8f647fb
ESKolpakov <eskolpakov@gmail.com>
2025-04-13 18:11:01 +0300

    Fix pyproject.toml

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-# Установка зависимостей и подготовка проекта
-make install
-make collectstatic
-make migrate
+# скачиваем uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
+
+# установка зависимостей, статики и миграций
+make install && make collectstatic && make migrate
