08a76ef7b295dfad7f80343724ab32a9c30211d2
ESKolpakov <eskolpakov@gmail.com>
2025-04-14 12:03:32 +0300

    small bug fix

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -55,8 +55,43 @@ class UserForm(UserCreationForm):
 
 
 class UserChangeForm(DjangoUserChangeForm):
-    password = None  # скрываем поле пароля в форме изменения
+    password = None  # скрываем оригинальное поле пароля
+
+    password1 = forms.CharField(
+        label="Пароль",
+        required=False,
+        help_text="Ваш пароль должен содержать как минимум 3 символа.",
+        widget=forms.PasswordInput(attrs={"class": "form-control"}),
+    )
+    password2 = forms.CharField(
+        label="Подтверждение пароля",
+        required=False,
+        help_text="Для подтверждения введите, пожалуйста, пароль ещё раз.",
+        widget=forms.PasswordInput(attrs={"class": "form-control"}),
+    )
 
     class Meta:
         model = User
         fields = ("first_name", "last_name", "username")
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get("password1")
+        password2 = cleaned_data.get("password2")
+
+        if password1 or password2:
+            if password1 != password2:
+                raise ValidationError("Пароли не совпадают.")
+            if len(password1) < 3:
+                raise ValidationError("Пароль должен содержать минимум 3 символа.")
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        password = self.cleaned_data.get("password1")
+        if password:
+            user.set_password(password)
+        if commit:
+            user.save()
+        return user
