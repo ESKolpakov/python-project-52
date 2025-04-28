f350c64b39115bc68fcbf66357625add728c86f1
ESKolpakov <eskolpakov@gmail.com>
2025-04-28 20:29:57 +0300

    fix forms.py

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Task
 
+
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
@@ -13,9 +14,9 @@ class TaskForm(forms.ModelForm):
             "labels": "Метки",
         }
         widgets = {
-            'name': forms.TextInput(attrs={'id': 'id_name'}),
-            'description': forms.Textarea(attrs={'id': 'id_description'}),
-            'status': forms.Select(attrs={'id': 'id_status'}),
-            'executor': forms.Select(attrs={'id': 'id_executor'}),
-            'labels': forms.SelectMultiple(attrs={'id': 'id_labels'}),
+            "name": forms.TextInput(attrs={"id": "id_name"}),
+            "description": forms.Textarea(attrs={"id": "id_description"}),
+            "status": forms.Select(attrs={"id": "id_status"}),
+            "executor": forms.Select(attrs={"id": "id_executor"}),
+            "labels": forms.SelectMultiple(attrs={"id": "id_labels"}),
         }
