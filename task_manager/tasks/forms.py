a5a3de70481177ca8cc2f2c51aa6120cae933831
ESKolpakov <eskolpakov@gmail.com>
2025-04-28 20:45:48 +0300

    fix forms.py and create.html

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,11 +1,23 @@
 from django import forms
 from .models import Task
 
-
 class TaskForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['status'].empty_label = "---------"
+        self.fields['executor'].empty_label = "---------"
+        self.fields['labels'].required = False
+
     class Meta:
         model = Task
         fields = ["name", "description", "status", "executor", "labels"]
+        widgets = {
+            "description": forms.Textarea(attrs={"rows": 3, "class": "form-control"}),
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "status": forms.Select(attrs={"class": "form-select"}),
+            "executor": forms.Select(attrs={"class": "form-select"}),
+            "labels": forms.SelectMultiple(attrs={"class": "form-select"}),
+        }
         labels = {
             "name": "Имя",
             "description": "Описание",
@@ -13,10 +25,6 @@ class TaskForm(forms.ModelForm):
             "executor": "Исполнитель",
             "labels": "Метки",
         }
-        widgets = {
-            "name": forms.TextInput(attrs={"id": "id_name"}),
-            "description": forms.Textarea(attrs={"id": "id_description"}),
-            "status": forms.Select(attrs={"id": "id_status"}),
-            "executor": forms.Select(attrs={"id": "id_executor"}),
-            "labels": forms.SelectMultiple(attrs={"id": "id_labels"}),
+        help_texts = {
+            "labels": "Удерживайте Ctrl для выбора нескольких меток",
         }
