a13985e8f74086cf6c478457e86c85c8291fd606
ESKolpakov <eskolpakov@gmail.com>
2025-04-29 18:45:47 +0300

    fix forms.py

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,23 +1,20 @@
 from django import forms
+from django.contrib.auth.models import User
 from .models import Task
 
+
 class TaskForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['status'].empty_label = "---------"
-        self.fields['executor'].empty_label = "---------"
-        self.fields['labels'].required = False
+    executor = forms.ModelChoiceField(
+        queryset=User.objects.all(),
+        label="Исполнитель",
+        required=False,
+        empty_label="---------",
+        to_field_name="id",
+    )
 
     class Meta:
         model = Task
         fields = ["name", "description", "status", "executor", "labels"]
-        widgets = {
-            "description": forms.Textarea(attrs={"rows": 3, "class": "form-control"}),
-            "name": forms.TextInput(attrs={"class": "form-control"}),
-            "status": forms.Select(attrs={"class": "form-select"}),
-            "executor": forms.Select(attrs={"class": "form-select"}),
-            "labels": forms.SelectMultiple(attrs={"class": "form-select"}),
-        }
         labels = {
             "name": "Имя",
             "description": "Описание",
@@ -25,6 +22,12 @@ class TaskForm(forms.ModelForm):
             "executor": "Исполнитель",
             "labels": "Метки",
         }
-        help_texts = {
-            "labels": "Удерживайте Ctrl для выбора нескольких меток",
+        widgets = {
+            "description": forms.Textarea(attrs={"rows": 3}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["executor"].label_from_instance = (
+            lambda obj: f"{obj.first_name} {obj.last_name}"
+        )
